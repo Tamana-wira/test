8031abb5b6c9017c9eea8305e49c3e7195fc41fc
Tamana <68454774+Tamana-wira@users.noreply.github.com>
2023-07-06 09:26:27 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// let age=22;
+
+// if(age<22){
+//     console.log("you are not old enough to enter");
+// }
+// else{
+//     console.log("welcome");
+// }
+// // 
+// let decribe=["Tamana",28,true]
+// decribe.push("wira")
+// console.log(decribe[3])
+let messages=["Hey hi","how are you","all good being working with you"]
+let newMessage="same here"
+messages.push(newMessage);
+console.log(messages[3])
+let lastItem=messages.pop();
+console.log(messages[2])
